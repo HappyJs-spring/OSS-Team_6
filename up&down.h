2583688203bfba7f2f0b012f03cfe4816dee79a6
Jiyou Hwang <fraguant5488@gmail.com>
2025-11-12 16:23:59 +0900

    Create up&down.h

---
+++ up&down.h
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+#define GAME_H
+
+#define MAX_ATTEMPTS 10
+#define MIN_NUM 1
+#define MAX_NUM 100
+
+
+int generate_random_number(void);
+int get_valid_input(void);
+void play_game(void);
+
+#endif
+#pragma once

c82669b2437422984279fe2e85b7f1fa30fff500
doseong :) <118369702+HappyJs-spring@users.noreply.github.com>
2025-12-10 15:29:42 +0900

    re

--- OSS/main.py
+++ OSS/main.py
@@ -70,7 +70,7 @@ status_img = pygame.transform.smoothscale(status_img, (STATUS_WIDTH, STATUS_HEIG
 
 dialogue_box = DialogueManager(screen, font)
 
-def display_story_text(text, nexttime = 600):
+def display_story_text(text, nexttime = 600, character=None):
     """DialogueManager를 사용하여 스토리 텍스트를 표시하고 사용자의 입력을 기다립니다."""
     
     dialogue_box.set_text(text)
@@ -104,22 +104,22 @@ def run_game(GameClass):
 
 
 def game_story_sequence():
-    # """게임의 순차적인 스토리를 정의하는 메인 함수"""
+    # # """게임의 순차적인 스토리를 정의하는 메인 함수"""
 
-    game_result_music = run_game(MusicGame)
-    if game_result_music == "QUIT":
-        return
+    # game_result_music = run_game(MusicGame)
+    # if game_result_music == "QUIT":
+    #     return
     
-    if game_result_music is True: # 행맨 승리
-        display_story_text("성공! 다음 단계를 진행.", 3000)
-    else: # 행맨 패배
-        display_story_text("실패. 게임 종료.", 4000)
-        return # 스토리 종료
+    # if game_result_music is True: # 행맨 승리
+    #     display_story_text("성공! 다음 단계를 진행.", 3000)
+    # else: # 행맨 패배
+    #     display_story_text("실패. 게임 종료.", 4000)
+    #     return # 스토리 종료
 
     # # 1  ------------------ <프롤로그> ---------------------------  
     # display_story_text("당신은 충북대학교 컴퓨터공학과 학생입니다. 당일 자정까지 전공과목의 기말대체 과제 제출이 있었으나 깜빡하고 제출하지 못했습니다. 해당 과제를 제출하지 못하면 당신은 F를 받고야 맙니다. 당신은 교수님 몰래 과제를 제출하기 위해 교수님들이 모두 퇴근하신 새벽에 전공 교수님 사무실이 위치한 공과대학 건물에 왔습니다.")
 
-    display_story_text("나 : (일부러 교수님이 모두 퇴근하신 시간대에 왔으니까. 과제 제출만하면 될꺼야!)")
+    display_story_text("나 : (일부러 교수님이 모두 퇴근하신 시간대에 왔으니까. 과제 제출만하면 될꺼야!)", character="president/president.png")
     display_story_text("(공대건물 4층으로 조용히 올라간다.)")
     display_story_text("(당신은 연구실 불이 켜져 있는 것을 보고 깜짝 놀란다.)")
     display_story_text("나 : 분명 이 시간엔 아무도 없을 거라 생각했는데, 누구지?")

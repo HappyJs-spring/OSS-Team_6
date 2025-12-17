74aadfea637d8136a17bb9bb51f2cd909a521523
doseong :) <118369702+HappyJs-spring@users.noreply.github.com>
2025-12-17 19:17:24 +0900

    Edit

--- OSS/main.py
+++ OSS/main.py
@@ -431,90 +431,90 @@ def game_story_sequence():
     # # """게임의 순차적인 스토리를 정의하는 메인 함수"""
 
     # 1  ------------------ <프롤로그> ---------------------------  
-    # display_story_text("당신은 충북대학교 컴퓨터공학과 학생입니다. 당일 자정까지 전공과목의 기말대체 과제 제출이 있었으나 깜빡하고 제출하지 못했습니다.")
-    # display_story_text("해당 과제를 제출하지 못하면 당신은 F를 받고야 맙니다.")
-    # display_story_text("당신은 교수님 몰래 과제를 제출하기 위해 교수님들이 모두 퇴근하신 새벽에 전공 교수님 사무실이 위치한 공과대학 건물에 왔습니다.")
+    display_story_text("당신은 충북대학교 컴퓨터공학과 학생입니다. 당일 자정까지 전공과목의 기말대체 과제 제출이 있었으나 깜빡하고 제출하지 못했습니다.")
+    display_story_text("해당 과제를 제출하지 못하면 당신은 F를 받고야 맙니다.")
+    display_story_text("당신은 교수님 몰래 과제를 제출하기 위해 교수님들이 모두 퇴근하신 새벽에 전공 교수님 사무실이 위치한 공과대학 건물에 왔습니다.")
 
-    # display_story_text("나 : (일부러 교수님이 모두 퇴근하신 시간대에 왔으니까. 과제 제출만하면 될꺼야!)", bg="e8-1(3rd stairs)")
-    # display_story_text("(공대건물 4층으로 조용히 올라간다.)", bg="e8-1(6)")
-    # display_story_text("(당신은 연구실 불이 켜져 있는 것을 보고 깜짝 놀란다.)")
-    # display_story_text("나 : 분명 이 시간엔 아무도 없을 거라 생각했는데, 누구지?")
-    # display_story_text("(당신은 연구실에서 교수님을 발견한다.)")
-    # display_story_text("나 : 이런 교수님이 아직도 퇴근하지 않으셨을 줄이야… 교수님 몰래 과제를 제출하고 빨리 나가야겠어..!")
+    display_story_text("나 : (일부러 교수님이 모두 퇴근하신 시간대에 왔으니까. 과제 제출만하면 될꺼야!)", bg="e8-1(3rd stairs)")
+    display_story_text("(공대건물 4층으로 조용히 올라간다.)", bg="e8-1(6)")
+    display_story_text("(당신은 연구실 불이 켜져 있는 것을 보고 깜짝 놀란다.)")
+    display_story_text("나 : 분명 이 시간엔 아무도 없을 거라 생각했는데, 누구지?")
+    display_story_text("(당신은 연구실에서 교수님을 발견한다.)")
+    display_story_text("나 : 이런 교수님이 아직도 퇴근하지 않으셨을 줄이야… 교수님 몰래 과제를 제출하고 빨리 나가야겠어..!")
 
     # 2.—---------------------------------------------
     # 업다운 게임
-    # display_story_text("(교수연구실 앞으로 이동한다.)", 600, bg="e8-1(2)")
-    # display_story_text("나 : 이런.. 교수연구실 문이 잠겨있잖아.. 7번 틀리면 경보가 울릴테니 그 전에 숫자를 맞춰야겠어..")
-    # display_story_text("교수실 문을 열어라!", 3000)
+    display_story_text("(교수연구실 앞으로 이동한다.)", 600, bg="e8-1(2)")
+    display_story_text("나 : 이런.. 교수연구실 문이 잠겨있잖아.. 7번 틀리면 경보가 울릴테니 그 전에 숫자를 맞춰야겠어..")
+    display_story_text("교수실 문을 열어라!", 3000)
     
-    # game_result_updown = run_game(UpDownGame)
+    game_result_updown = run_game(UpDownGame)
     
-    # if game_result_updown == "QUIT":
-    #     return
+    if game_result_updown == "QUIT":
+        return
         
-    # if game_result_updown is True: # 업다운 승리
-    #     display_story_text("잠금해제! 잠긴 문이 열립니다!", 3000)
-    # else: # 업다운 패배
-    #     display_story_text('자네 거기서 지금 뭐하는건가!', ch="professor_angry")
-    #     display_story_text("문을 여는데 실패했습니다.. 교수님게 발각되어 학점 F를 받게되었습니다.", 2000)
-    #     show_ending(screen, "fail", display_story_text)
-    #     return
+    if game_result_updown is True: # 업다운 승리
+        display_story_text("잠금해제! 잠긴 문이 열립니다!", 3000)
+    else: # 업다운 패배
+        display_story_text('자네 거기서 지금 뭐하는건가!', ch="professor_angry")
+        display_story_text("문을 여는데 실패했습니다.. 교수님게 발각되어 학점 F를 받게되었습니다.", 2000)
+        show_ending(screen, "fail", display_story_text)
+        return
 
-    # # 3.—---------------------------------------------
-    # # 행맨게임
-    # display_story_text("(교수실 문을 여는데 성공했다.)")
-    # display_story_text("나 : 후.. 운 좋게 성공했다.")
-    # display_story_text("나 : 그나저나. 과제물이 있는 케비넷이 어딨지..?")
-    # display_story_text("나 : 찾았다!")
-    # display_story_text("(과제물 케비넷이 잠겨있다.)")
+    # 3.—---------------------------------------------
+    # 행맨게임
+    display_story_text("(교수실 문을 여는데 성공했다.)")
+    display_story_text("나 : 후.. 운 좋게 성공했다.")
+    display_story_text("나 : 그나저나. 과제물이 있는 케비넷이 어딨지..?")
+    display_story_text("나 : 찾았다!")
+    display_story_text("(과제물 케비넷이 잠겨있다.)")
 
-    # display_story_text("나 : 이런..! 이번엔 영문자물쇠네.. 오랜 시간을 지체하면 교수님께 들킬거야..!")
+    display_story_text("나 : 이런..! 이번엔 영문자물쇠네.. 오랜 시간을 지체하면 교수님께 들킬거야..!")
  
-    # game_result_hangman = run_game(HangmanGame)
-    # if game_result_hangman == "QUIT":
-    #     return
+    game_result_hangman = run_game(HangmanGame)
+    if game_result_hangman == "QUIT":
+        return
     
-    # if game_result_hangman is True: # 행맨 승리
-    #     display_story_text("(철컥)")
-    #     display_story_text("열렸다!")
-    #     display_story_text("(자물쇠를 해제 하였습니다)", 3000)
-    # else: # 행맨 패배
-    #     display_story_text("자네 거기서 지금 뭐하는건가!", ch="professor_angry")
-    #     display_story_text("문을 여는데 실패했습니다.. 교수님게 발각되어 학점 F를 받게되었습니다.", 2000)
-    #     show_ending(screen, "fail", display_story_text)
-    #     return # 스토리 종료
+    if game_result_hangman is True: # 행맨 승리
+        display_story_text("(철컥)")
+        display_story_text("열렸다!")
+        display_story_text("(자물쇠를 해제 하였습니다)", 3000)
+    else: # 행맨 패배
+        display_story_text("자네 거기서 지금 뭐하는건가!", ch="professor_angry")
+        display_story_text("문을 여는데 실패했습니다.. 교수님게 발각되어 학점 F를 받게되었습니다.", 2000)
+        show_ending(screen, "fail", display_story_text)
+        return # 스토리 종료
     
-    # # # 4.—---------------------------------------------
-    # # # 올바른 대화 선택지
+    # # 4.—---------------------------------------------
+    # # 올바른 대화 선택지
     
-    # display_story_text("(무사히 과제를 제출하고 교수연구실 밖으로 나왔다.)")
-    # display_story_text("나 : 후.. 이번에도 운이 좋았어.. 이제 빨리 나가야겠다.")
-    # display_story_text("(복도 끝에서 교수연구실 쪽으로 걸어오는 발소리가 들린다.)", bg="e8-1(6)")
-    # display_story_text("나 : 누군가 온다..! 숨어야 해!", bg="e8-1(1)")
-    # display_story_text("(급하게 오픈소스SW 강의실로 몸을 숨긴다.)", bg="e8-1(4)")
-    # display_story_text("??? : 이 시간에 왜 강의실에 불이 켜져 있지?", ch="professor")
-    # display_story_text("(???이 들어온다.)")
-    # display_story_text("전공교수님 : 자네. 이 시간까지 강의실에서 뭐하는 건가?")
+    display_story_text("(무사히 과제를 제출하고 교수연구실 밖으로 나왔다.)")
+    display_story_text("나 : 후.. 이번에도 운이 좋았어.. 이제 빨리 나가야겠다.")
+    display_story_text("(복도 끝에서 교수연구실 쪽으로 걸어오는 발소리가 들린다.)", bg="e8-1(6)")
+    display_story_text("나 : 누군가 온다..! 숨어야 해!", bg="e8-1(1)")
+    display_story_text("(급하게 오픈소스SW 강의실로 몸을 숨긴다.)", bg="e8-1(4)")
+    display_story_text("??? : 이 시간에 왜 강의실에 불이 켜져 있지?", ch="professor")
+    display_story_text("(???이 들어온다.)")
+    display_story_text("전공교수님 : 자네. 이 시간까지 강의실에서 뭐하는 건가?")
 
-    # choice1 = choice_dialogue([
-    # "강의실에 남아 공부하고 있었다고 이야기한다.",
-    # "과제 제출하러 왔다고 말한다."
-    # ])
+    choice1 = choice_dialogue([
+    "강의실에 남아 공부하고 있었다고 이야기한다.",
+    "과제 제출하러 왔다고 말한다."
+    ])
 
-    # if choice1 == 0:
-    #         display_story_text("나 : 강의실에 남아서 복습하고 있었습니다.", ch="professor_smile")
-    #         display_story_text("전공교수님 : 훌륭한 학생이군. 열심히 하게.", ch="clear")
-    #         display_story_text("(전공 교수님이 밖으로 나간다.)")
-    #         display_story_text("나 : (휴… 살았다..)")
+    if choice1 == 0:
+            display_story_text("나 : 강의실에 남아서 복습하고 있었습니다.", ch="professor_smile")
+            display_story_text("전공교수님 : 훌륭한 학생이군. 열심히 하게.", ch="clear")
+            display_story_text("(전공 교수님이 밖으로 나간다.)")
+            display_story_text("나 : (휴… 살았다..)")
 
-    # elif choice1 == 1:
-    #     display_story_text("나 : 과제 제출하러 왔습니다.", ch="professor_angry")
-    #     display_story_text("전공교수 : 과제제출은 어제까지 인걸로 알고있는데..?")
-    #     display_story_text("나 : 하하.. 들켰네.")
-    #     display_story_text("교수님게 발각되어 학점 F를 받게되었습니다.", 2000)
-    #     show_ending(screen, "fail", display_story_text)        
-    #     return
+    elif choice1 == 1:
+        display_story_text("나 : 과제 제출하러 왔습니다.", ch="professor_angry")
+        display_story_text("전공교수 : 과제제출은 어제까지 인걸로 알고있는데..?")
+        display_story_text("나 : 하하.. 들켰네.")
+        display_story_text("교수님게 발각되어 학점 F를 받게되었습니다.", 2000)
+        show_ending(screen, "fail", display_story_text)        
+        return
 
 
     # # 5.—---------------------------------------------
